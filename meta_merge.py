7f61c382cdc7af463eb75756e9eec8784ecee034
mareep <62206240+vveg26@users.noreply.github.com>
2023-09-08 14:57:00 +0800

    Update meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -1,187 +1,203 @@
 import yaml
 import codecs
 import json
+import urllib.request
 # 定义一个空列表用于存储合并后的代理配置
 merged_proxies = []
+# 获取clash文本中的内容
+try:
+    with open('./urls/clash_urls.txt', 'r') as file:
+        urls = file.read().splitlines()
 
-# 从reality文本文件中读取网址
-with open('./urls/clash_urls.txt', 'r') as file:
-    urls = file.read().splitlines()
+    # 遍历每个网址
+    for url in urls:
+        try:
+            # 使用适当的方法从网址中获取内容，这里使用urllib库示例
+            response = urllib.request.urlopen(url)
+            data = response.read().decode('utf-8')
 
-# 遍历每个网址
-for url in urls:
-    # 使用适当的方法从网址中获取内容，这里使用urllib库示例
-    import urllib.request
-    response = urllib.request.urlopen(url)
-    data = response.read().decode('utf-8')
+            # 解析YAML格式的内容
+            content = yaml.safe_load(data)
 
-    # 解析YAML格式的内容
-    content = yaml.safe_load(data)
-
-    # 提取proxies部分并合并到merged_proxies中
-    proxies = content.get('proxies', [])
-    merged_proxies.extend(proxies)
+            # 提取proxies部分并合并到merged_proxies中
+            proxies = content.get('proxies', [])
+            merged_proxies.extend(proxies)
+        except Exception as e:
+            print(f"Error processing URL {url}: {e}")
+except Exception as e:
+    print(f"Error reading file: {e}")
 
 
-# shadowtls节点处理
-# 从文件中读取shadowtls_urls.txt中的网址列表
-with open("./urls/shadowtls_urls.txt", "r") as file:
-    urls = file.read().splitlines()
+# shadowtls singbox节点处理
+try:
+    with open("./urls/shadowtls_urls.txt", "r") as file:
+        urls = file.read().splitlines()
 
-# 遍历每个网址
-for index, url in enumerate(urls):
-    # 使用适当的方法从网址中获取内容，这里使用urllib库示例
-    import urllib.request
-    response = urllib.request.urlopen(url)
-    data = response.read().decode("utf-8")
-    json_data = json.loads(data)
-    
-    # 提取所需字段
-    method = json_data["outbounds"][0]["method"]
-    password = json_data["outbounds"][0]["password"]
-    server = json_data["outbounds"][1]["server"]
-    server_port = json_data["outbounds"][1]["server_port"]
-    server_name = json_data["outbounds"][1]["tls"]["server_name"]
-    name = f"shadowtls{index}"
-    # 创建当前网址的proxy字典
-    proxy = {
-        "name": name,
-        "type": "ss",
-        "server": server,
-        "port": server_port,
-        "cipher": method,
-        "password": password,
-        "plugin": "shadow-tls",
-        "client-fingerprint": "chrome",
-        "plugin-opts": {
-            "host": server_name,
-            "password": "",
-            "version": 1
+    # 遍历每个网址
+    for index, url in enumerate(urls):
+        try:
+            # 使用适当的方法从网址中获取内容，这里使用urllib库示例
+            response = urllib.request.urlopen(url)
+            data = response.read().decode("utf-8")
+            json_data = json.loads(data)
 
-        }
-    }
-    
-    # 将当前proxy字典添加到所有proxies列表中
-    merged_proxies.append(proxy)
+            # 提取所需字段
+            method = json_data["outbounds"][0]["method"]
+            password = json_data["outbounds"][0]["password"]
+            server = json_data["outbounds"][1]["server"]
+            server_port = json_data["outbounds"][1]["server_port"]
+            server_name = json_data["outbounds"][1]["tls"]["server_name"]
+            name = f"shadowtls{index}"
+            # 创建当前网址的proxy字典
+            proxy = {
+                "name": name,
+                "type": "ss",
+                "server": server,
+                "port": server_port,
+                "cipher": method,
+                "password": password,
+                "plugin": "shadow-tls",
+                "client-fingerprint": "chrome",
+                "plugin-opts": {
+                    "host": server_name,
+                    "password": "",
+                    "version": 1
+                }
+            }
 
-# 歇斯底里节点处理
+            # 将当前proxy字典添加到所有proxies列表中
+            merged_proxies.append(proxy)
+        except Exception as e:
+            print(f"Error processing URL {url}: {e}")
+except Exception as e:
+    print(f"Error reading file: {e}")
+#歇斯底里节点 json处理
+try:
+    with open("./urls/hysteria_urls.txt", "r") as file:
+        urls = file.read().splitlines()
 
-with open("./urls/hysteria_urls.txt", "r") as file:
-    urls = file.read().splitlines()
+    # 遍历每个网址
+    for index, url in enumerate(urls):
+        try:
+            # 使用适当的方法从网址中获取内容，这里使用urllib库示例
+            response = urllib.request.urlopen(url)
+            data = response.read().decode("utf-8")
+            json_data = json.loads(data)
 
-# 遍历每个网址
-for index, url in enumerate(urls):
-    # 使用适当的方法从网址中获取内容，这里使用urllib库示例
-    import urllib.request
-    response = urllib.request.urlopen(url)
-    data = response.read().decode("utf-8")
-    json_data = json.loads(data)
-    
-    # 提取所需字段
-    auth = json_data["auth_str"]
-    string = json_data["server"]
-    result = string.split(":")
-    server = result[0]
-    server_port = result[1]
-    server_name = json_data["server_name"]
-    alpn = json_data["alpn"]
-    protocol = json_data["protocol"]
-    name = f"hysteria{index}"
-    
-    # 创建当前网址的proxy字典
-    proxy = {
-        "name": name,
-        "type": "hysteria",
-        "server": server,
-        "port": server_port,
-        "auth_str": auth,
-        "up": 11, 
-        "down": 55,
-        "fast-open": True, 
-        "protocol": protocol, 
-        "sni": server_name, 
-        "skip-cert-verify": True, 
-        "alpn": [alpn]
-    }
-    
-    # 将当前proxy字典添加到所有proxies列表中
-    merged_proxies.append(proxy)
+            # 提取所需字段
+            auth = json_data["auth_str"]
+            string = json_data["server"]
+            result = string.split(":")
+            server = result[0]
+            server_port = result[1]
+            server_name = json_data["server_name"]
+            alpn = json_data["alpn"]
+            protocol = json_data["protocol"]
+            name = f"hysteria{index}"
 
-# reality节点处理
-print("提取reality")
-with open("./urls/reality_urls.txt", "r") as file:
-    urls = file.read().splitlines()
+            # 创建当前网址的proxy字典
+            proxy = {
+                "name": name,
+                "type": "hysteria",
+                "server": server,
+                "port": server_port,
+                "auth_str": auth,
+                "up": 11,
+                "down": 55,
+                "fast-open": True,
+                "protocol": protocol,
+                "sni": server_name,
+                "skip-cert-verify": True,
+                "alpn": [alpn]
+            }
 
-# 遍历每个网址
-for index, url in enumerate(urls):
-    # 使用适当的方法从网址中获取内容，这里使用urllib库示例
-    import urllib.request
-    response = urllib.request.urlopen(url)
-    data = response.read().decode("utf-8")
-    json_data = json.loads(data)
-    
-    # 提取所需字段
-    type = json_data["outbounds"][0]["protocol"]
-    server = json_data["outbounds"][0]["settings"]["vnext"][0]["address"]
-    port = json_data["outbounds"][0]["settings"]["vnext"][0]["port"]
-    uuid = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["id"]
-    flow = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["flow"]
-    network = json_data["outbounds"][0]["streamSettings"]["network"]
-    publicKey = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["publicKey"]
-    shortId = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["shortId"]
-    serverName = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["serverName"]
-    fingerprint = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["fingerprint"]
-    name = f"reality{index}"
-    # 根据network判断tcp
-    if network == "tcp":
-        proxy = {
-            "name": name,
-            "type": type,
-            "server": server,
-            "port": port,
-            "uuid": uuid,
-            "network": network,
-            "tls": True,
-            "udp": True,
-            "flow": flow,
-            "client-fingerprint": fingerprint,
-            "servername": serverName,                
-            "reality-opts":{
-                "public-key": publicKey,
-                "short-id": shortId}
+            # 将当前proxy字典添加到所有proxies列表中
+            merged_proxies.append(proxy)
+        except Exception as e:
+            print(f"Error processing URL {url}: {e}")
+except Exception as e:
+    print(f"Error reading file: {e}")
 
-        }
-    # 根据network判断
-    elif network == "grpc":
 
-        serviceName = json_data["outbounds"][0]["streamSettings"]["grpcSettings"]["serviceName"]
+# xray json reality节点处理
+try:
+    with open("./urls/reality_urls.txt", "r") as file:
+        urls = file.read().splitlines()
 
-        # 创建当前网址的proxy字典
-        proxy = {
-            "name": name,
-            "type": type,
-            "server": server,
-            "port": port,
-            "uuid": uuid,
-            "network": network,
-            "tls": True,
-            "udp": True,
-            "flow": flow,
-            "client-fingerprint": fingerprint,
-            "servername": serverName,
-            "grpc-opts":{
-                "grpc-service-name": serviceName
-                },
+    # 遍历每个网址
+    for index, url in enumerate(urls):
+        try:
+            # 使用适当的方法从网址中获取内容，这里使用urllib库示例
+            response = urllib.request.urlopen(url)
+            data = response.read().decode("utf-8")
+            json_data = json.loads(data)
+
+            # 提取所需字段
+            type = json_data["outbounds"][0]["protocol"]
+            server = json_data["outbounds"][0]["settings"]["vnext"][0]["address"]
+            port = json_data["outbounds"][0]["settings"]["vnext"][0]["port"]
+            uuid = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["id"]
+            flow = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["flow"]
+            network = json_data["outbounds"][0]["streamSettings"]["network"]
+            publicKey = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["publicKey"]
+            shortId = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["shortId"]
+            serverName = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["serverName"]
+            fingerprint = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["fingerprint"]
+            name = f"reality{index}"
+            
+            # 根据network判断tcp
+            if network == "tcp":
+                proxy = {
+                    "name": name,
+                    "type": type,
+                    "server": server,
+                    "port": port,
+                    "uuid": uuid,
+                    "network": network,
+                    "tls": True,
+                    "udp": True,
+                    "flow": flow,
+                    "client-fingerprint": fingerprint,
+                    "servername": serverName,                
+                    "reality-opts":{
+                        "public-key": publicKey,
+                        "short-id": shortId}
+                }
                 
-            "reality-opts":{
-                "public-key": publicKey,
-                "short-id": shortId}
-
-        }
-    else:
-        break
-    # 将当前proxy字典添加到所有proxies列表中
-    merged_proxies.append(proxy)
+            # 根据network判断grpc
+            elif network == "grpc":
+                serviceName = json_data["outbounds"][0]["streamSettings"]["grpcSettings"]["serviceName"]
+                
+                # 创建当前网址的proxy字典
+                proxy = {
+                    "name": name,
+                    "type": type,
+                    "server": server,
+                    "port": port,
+                    "uuid": uuid,
+                    "network": network,
+                    "tls": True,
+                    "udp": True,
+                    "flow": flow,
+                    "client-fingerprint": fingerprint,
+                    "servername": serverName,
+                    "grpc-opts":{
+                        "grpc-service-name": serviceName
+                    },
+                    "reality-opts":{
+                        "public-key": publicKey,
+                        "short-id": shortId}
+                }
+            else:
+                print(f"Unknown network type for URL {url}")
+                continue
+            
+            # 将当前proxy字典添加到所有proxies列表中
+            merged_proxies.append(proxy)
+        except Exception as e:
+            print(f"Error processing URL {url}: {e}")
+except Exception as e:
+    print(f"Error reading file: {e}")
 
 
 
